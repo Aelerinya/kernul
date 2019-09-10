f91a2adac1342c9485729cb20e9cf8dbeff9d174
Julien Philippon <julien.philippon@epitech.eu>
2019-09-10 20:58:26 +0200

    Create simple GDT table structures

---
+++ src/gdt.rs
@@ -0,0 +1,110 @@
+use bitfield::bitfield;
+use bitflags::bitflags;
+use lazy_static::lazy_static;
+
+bitflags! {
+    /// Bit flags describing the access permissions of a data segment
+    pub struct GdtAccess: u8 {
+        const PRESENT = 0b10000000;
+        const USER_RING = 0b01100000;
+        const CODE_SEGMENT = 0b00011000;
+        const DATA_SEGMENT = 0b00010000;
+        const DIRECTION_DOWN = 0b100;
+        const CONFORMING = 0b100;
+        const CODE_READABLE = 0b10;
+        const DATA_WRITABLE = 0b10;
+        const ACCESSED = 0b1;
+    }
+}
+
+impl From<u8> for GdtAccess {
+    fn from(value: u8) -> GdtAccess {
+        GdtAccess::from_bits(value).expect("Invalid GDT access flags")
+    }
+}
+
+bitflags! {
+    /// Various flags of a data segment: granularity and size
+    pub struct GdtFlags: u8 {
+        const PAGE_GRANULARITY = 0b1000;
+        const PROTECTED_MODE = 0b0100;
+        const LONG_MODE = 0b0010;
+    }
+}
+impl From<u8> for GdtFlags {
+    fn from(value: u8) -> GdtFlags {
+        GdtFlags::from_bits(value).expect("Invalid GDT flags")
+    }
+}
+
+bitfield! {
+    /// Structure describing a entry in the Global Descriptor Table (GDT)
+    /// It contains the base of the segment, its size (limit), its access permissions and flags
+    #[derive(Clone, Copy)]
+    pub struct GdtEntry(u64);
+    impl Debug;
+    u16, limit_low, set_limit_low: 15, 0;
+    u16, base_low, set_base_low: 31, 16;
+    u8, base_middle, set_base_middle: 39, 32;
+    u8, into GdtAccess, access, set_access: 47, 40;
+    u8, limit_high, set_limit_high: 51, 48;
+    u8, into GdtFlags, flags, set_flags: 55, 52;
+    u8, base_high, set_base_high: 63, 56;
+}
+
+/// Error returned when a GDT entry is malformed
+#[derive(Debug)]
+pub enum GdtError {
+    InvalidLimit,
+}
+
+impl GdtEntry {
+    /// Creates a new GDT entry
+    pub fn new(
+        base: u64,
+        limit: u32,
+        mut access: GdtAccess,
+        flags: GdtFlags,
+    ) -> Result<GdtEntry, GdtError> {
+        let mut entry = GdtEntry(0);
+        entry.set_base(base);
+        entry.set_limit(limit)?;
+        // Set the present flag
+        access |= GdtAccess::PRESENT;
+        entry.set_access(access.bits());
+        entry.set_flags(flags.bits());
+        Ok(entry)
+    }
+
+    fn set_base(&mut self, base: u64) {
+        self.set_base_low((base & 0xFFFF) as u16);
+        self.set_base_middle((base >> 16 & 0xFF) as u8);
+        self.set_base_high((base >> 24 & 0xFF) as u8);
+    }
+
+    fn set_limit(&mut self, limit: u32) -> Result<(), GdtError> {
+        if limit > 0xFFFFF {
+            Err(GdtError::InvalidLimit)
+        } else {
+            self.set_limit_low((limit & 0xFFFF) as u16);
+            self.set_limit_high((limit >> 16 & 0xFF) as u8);
+            Ok(())
+        }
+    }
+}
+
+lazy_static! {
+    static ref GDT_TABLE: [GdtEntry; 4] = [
+        // Null entry
+        GdtEntry::new(0, 0, GdtAccess::empty(), GdtFlags::PROTECTED_MODE).unwrap(),
+        // Code segment
+        GdtEntry::new(0, 0xFFFFF, GdtAccess::CODE_SEGMENT | GdtAccess::CODE_READABLE, GdtFlags::PROTECTED_MODE).unwrap(),
+        // Data segment
+        GdtEntry::new(0, 0xFFFFF, GdtAccess::DATA_SEGMENT | GdtAccess::DATA_WRITABLE, GdtFlags::PROTECTED_MODE).unwrap(),
+        // TSS segment
+        GdtEntry::new(0, 0, GdtAccess::ACCESSED, GdtFlags::PROTECTED_MODE).unwrap()
+    ];
+}
+
+/// Initializes the GDT table and loads it into memory
+fn init_gdt() {}
